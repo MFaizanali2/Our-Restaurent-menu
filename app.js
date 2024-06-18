976b831810566b146cfb0f65fd0813a3afd4ea21
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-19 00:31:18 +0500

    js line 10
    continue

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let body = document.querySelector("body");
 body.style.backgroundColor = "gray";
+
+let head = document.querySelector("#head");
+let para = document.querySelector("#para");
+
+function change(){
+    head.innerHTML = "Hello faizi";
+    para.innerHTML = `My name is Faizi and I am a web developer and I am excited to Learn data Science`;
+}
